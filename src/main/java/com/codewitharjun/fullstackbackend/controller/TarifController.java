7b72f3581e50e3b76d2d202d881ef7184926c4ef
H.Geraud <hounguegeraud@gmail.com>
2024-04-22 10:57:31 +0100

    conversion du type de retour en json

--- src/main/java/com/codewitharjun/fullstackbackend/controller/TarifController.java
+++ src/main/java/com/codewitharjun/fullstackbackend/controller/TarifController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@CrossOrigin("http://localhost:3000")
+@CrossOrigin("*")
 public class TarifController {
 
 
@@ -19,34 +19,48 @@ public class TarifController {
     @Autowired
     private TarifRepository tarifRepository;
 
-    @PostMapping("/tarif")
+    @PostMapping("/api/tarif")
     Tarifsw newTarif(@RequestBody Tarifsw newTarifsw) {
         return tarifRepository.save(newTarifsw);
     }
 
-    @GetMapping("/tarifs")
+    @GetMapping("/api/tarifs")
     List<Tarifsw> getAllTarif() {
         return tarifRepository.findAll();
     }
 
-   @GetMapping("/tarif/{nomenclature}")
+   @GetMapping("/api/tarif/{nomenclature}")
    Object getTarifByNomenclature(@PathVariable Integer nomenclature) {
         return tarifRepository.findByNomenclature(nomenclature)
                 .orElseThrow(() -> new TarifNotFoundException(nomenclature));
     }
 
 
+    class tarifsw {
+        String libelle ;
 
-    @GetMapping("/tariflibelle/{Libellenomenclature}")
-    String getLibelleByNomenclature(@PathVariable("Libellenomenclature") Integer nomenclature) {
+        public tarifsw(String libelle) {
+            this.libelle = libelle;
+        }
+
+        public String getLibelle() {
+            return libelle;
+        }
+
+        public void setLibelle(String libelle) {
+            this.libelle = libelle;
+        }
+    }
+    @GetMapping("/api/tariflibelle/{Libellenomenclature}")
+    Object getLibelleByNomenclature(@PathVariable("Libellenomenclature") Integer nomenclature) {
         Tarifsw tarifsw = (Tarifsw) tarifRepository.findByNomenclature(nomenclature)
                 .orElseThrow(() -> new TarifNotFoundException(nomenclature));
-
-        return tarifsw.getLibelle();
+        System.out.println("le libeller voulu  est " + tarifsw.getLibelle());
+        return new tarifsw(tarifsw.getLibelle());
     }
 
 
-    @GetMapping("/tarif/{nomenclature}/taux")
+    @GetMapping("/api/tarif/{nomenclature}/taux")
     double getTauxByNomenclature(@PathVariable Integer nomenclature) {
         Tarifsw tarifsw = (Tarifsw) tarifRepository.findByNomenclature(nomenclature)
                 .orElseThrow(() -> new TarifNotFoundException(nomenclature));
@@ -65,8 +79,7 @@ public class TarifController {
         if (ps == null || pcs == null || pc == null || rs == null || rau == null || ect == null || da == null || tva == null) {
             throw new IllegalStateException("Une ou plusieurs valeur sont null");
         }
-
-         double tauxda = ((((pc+pcs+ps+dd+rs+rau+ect)+100)/100)*da);
+        double tauxda = ((((pc+pcs+ps+dd+rs+rau+ect)+100)/100)*da);
         double tauxaid = ((((pc+pcs+ps+dd+rs+rau+ect) +(((((pc+pcs+ps+dd+rs+rau+ect)+100)/100)*da))+100)/100)*1);
         double tauxtva =   ((((pc+pcs+ps+dd+rs+rau) + (((((pc+pcs+ps+dd+rs+rau+ect)+100)/100)*da))+100)/100)*tva);
 
--- src/main/java/com/codewitharjun/fullstackbackend/repository/TarifRepository.java
+++ src/main/java/com/codewitharjun/fullstackbackend/repository/TarifRepository.java
@@ -7,4 +7,5 @@ import java.util.Optional;
 
 public interface TarifRepository extends JpaRepository<Tarifsw,Integer> {
  Optional<Object> findByNomenclature(Integer nomenclature);
+
 }
