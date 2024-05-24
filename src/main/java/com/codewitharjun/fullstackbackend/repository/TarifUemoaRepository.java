3ffa662057d4bd1273136d69a3a0cc5fd32bb415
H.Geraud <hounguegeraud@gmail.com>
2024-05-24 10:17:57 +0100

    taux Lineaire uemoa

---
+++ src/main/java/com/codewitharjun/fullstackbackend/repository/TarifUemoaRepository.java
@@ -0,0 +1,13 @@
+package com.codewitharjun.fullstackbackend.repository;
+
+import com.codewitharjun.fullstackbackend.model.Tarifsw;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface TarifUemoaRepository extends JpaRepository<Tarifsw,String> {
+        Optional<Object> findByNomenclature(String nomenclature);
+
+        Optional<Object> getTauxLineaireByNomenclature(String nomenclature);
+
+        }
